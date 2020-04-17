5b33288efcd19bc45ca4adde81d331945d48355e
dukejiang <jyx19980308@gmail.com>
2020-04-16 21:35:53 -0500

    "save"

--- src/client/main.js
+++ src/client/main.js
@@ -74,48 +74,48 @@ const MyApp = () => {
   };
 
   return (
-    <BrowserRouter>
-      <GridBase>
-        <Header user={state.username} email={state.primary_email} />
-        <Route exact path="/" component={Landing} />
-        <Route
-          path="/login"
-          render={p =>
-            loggedIn() ? (
-              <Redirect to={`/profile/${state.username}`} />
-            ) : (
-              <Login {...p} logIn={logIn} />
-            )
-          }
-        />
-        <Route path="/logout" render={p => <Logout {...p} logOut={logOut} />} />
-        <Route
-          path="/register"
-          render={p => {
-            return loggedIn() ? (
-              <Redirect to={`/profile/${state.username}`} />
-            ) : (
-              <Register {...p} />
-            );
-          }}
-        />
-        <Route
-          path="/profile/:username"
-          render={p => <Profile {...p} currentUser={state.username} />}
-        />
-        <Route
-          path="/start"
-          render={p => {
-            return loggedIn() ? <Start {...p} /> : <Redirect to={'/login'} />;
-          }}
-        />
-        <Route path="/game/:id" render={p => <Game {...p} user={p.user} />} />
-        <Route
-          path="/results/:id"
-          render={p => <Results {...p} user={p.user} />}
-        />
-      </GridBase>
-    </BrowserRouter>
+      <BrowserRouter>
+        <GridBase>
+          <Header user={state.username} email={state.primary_email} />
+          <Route exact path="/" component={Landing} />
+          <Route
+              path="/login"
+              render={p =>
+                  loggedIn() ? (
+                      <Redirect to={`/profile/${state.username}`} />
+                  ) : (
+                      <Login {...p} logIn={logIn} />
+                  )
+              }
+          />
+          <Route path="/logout" render={p => <Logout {...p} logOut={logOut} />} />
+          <Route
+              path="/register"
+              render={p => {
+                return loggedIn() ? (
+                    <Redirect to={`/profile/${state.username}`} />
+                ) : (
+                    <Register {...p} />
+                );
+              }}
+          />
+          <Route
+              path="/profile/:username"
+              render={p => <Profile {...p} currentUser={state.username} />}
+          />
+          <Route
+              path="/start"
+              render={p => {
+                return loggedIn() ? <Start {...p} /> : <Redirect to={'/login'} />;
+              }}
+          />
+          <Route path="/game/:id" render={p => <Game {...p} user={p.user} />} />
+          <Route
+              path="/results/:id"
+              render={p => <Results {...p} user={p.user} />}
+          />
+        </GridBase>
+      </BrowserRouter>
   );
 };
 
